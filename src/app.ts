6b5b5550eb197a4d11f54177d2c3f3f717b74c9d
Lamodot <lamodots@gmail.com>
2024-07-30 10:35:05 -0700

    API documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser';
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { adminRouter } from './routes/admin/authRoutes';
 
 export const app: Express = express();
 app.use(morgan('dev'));
@@ -12,3 +13,5 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
 	res.send({ message: 'Welcome to OrderUp API' });
 });
+
+app.use('/api/v1/auth', adminRouter);

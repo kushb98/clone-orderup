d8d454f2217444ab12a64fa2919385d242ef6ced
Heet Sakaria <heetsakaria@gmail.com>
2024-08-06 20:25:54 -0600

    created logic for login

--- src/controllers/admin/authControllers.ts
+++ src/controllers/admin/authControllers.ts
@@ -1,8 +1,41 @@
 import { Request, Response } from 'express-serve-static-core';
+import authDAO from '../../dao/authDao'; // Adjust the path to where your authDAO is located
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
-export const loginController = (req: Request, res: Response) => {
-	res.status(200).json({ success: true, message: 'Loggedin!!' });
+export const loginController = async (req: Request, res: Response) => {
+	try {
+	  // Extract username and password
+	  const { username, password } = req.body;
+  
+	  // DB call to find the user
+	  const user = await authDAO.findAdminByUsername(username);
+  
+    // Check if user is found and password is present
+    if (!user || !user.password) {
+		return res.status(401).json({ success: false, message: 'Invalid username or password' });
+	  }
+	  // Assuming password is hashed using bcrypt
+	  const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+	  if (!isPasswordValid) {
+		return res.status(401).json({ success: false, message: 'Invalid username or password' });
+	  }
+  
+	  // Create access token
+	  const accessToken = jwt.sign(
+		{ userId: user._id, username: user.username },
+		process.env.JWT_SECRET as string,
+		{ expiresIn: process.env.JWT_EXPIRES_IN }
+	  );
+  
+	  res.status(200).json({ success: true, message: 'Logged in successfully', user, accessToken });
+	} catch (error) {
+	  console.error('Login error:', error);
+	  res.status(500).json({ success: false, message: 'An error occurred during login' });
+	}
 };
+
 export const logoutController = (req: Request, res: Response) => {
 	res.status(200).json({ success: true, message: 'Logged out!!' });
 };

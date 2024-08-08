f1ffbe35403fe509ea9efc76f893b14029101382
Deeptha <tmj.deeptha@gmail.com>
2024-08-07 18:40:28 -0700

    error comments

--- src/utils/NotFoundError.ts
+++ src/utils/NotFoundError.ts
@@ -1,8 +1,18 @@
 import AppError from './AppError';
 
+/**
+ * Custom error class for handling 404 Not Found errors.
+ * Extends the AppError class.
+ */
 class NotFoundError extends AppError {
-    constructor(message:string){
-        super(message, 404)
+    /**
+     * Constructor for the NotFoundError class.
+     * 
+     * @param {string} message - The error message.
+     */
+    constructor(message: string) {
+        // Call the parent class (AppError) constructor with the message and a 404 status code.
+        super(message, 404);
     }
 }
 
